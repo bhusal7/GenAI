9559c32186aabb06d4c89e7ff678d55bc8dcecc0
bhusal7 <pradeepbbusal@gmail.com>
2026-07-21 21:32:35 +0545

    Update

--- RAG/documents_loader/page.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-data = WebBaseLoader("https://www.apple.com/mac/")
-
-docs = data.load()
-
-print(docs[0].page_content)
-
-print(len(docs))
