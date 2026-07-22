eaf30f3fb665049e653786cef5294bfccaad7cab
bhusal7 <pradeepbbusal@gmail.com>
2026-07-22 22:36:13 +0545

    .

--- AICodeAssistance/AI_Code_Assistant/RAG/embeddings.py
+++
@@ -1,4 +0,0 @@
-from config import embeddings
-
-def get_embeddings():
-    return embeddings
--- AICodeAssistance/AI_Code_Assistant/RAG/retriever.py
+++
@@ -1,9 +0,0 @@
-from config import SEARCH_TYPE, SEARCH_KWARGS
-from langchain_core.vectorstores import VectorStoreRetriever
-
-def get_retriever(vectordb) -> VectorStoreRetriever:
-    
-    return vectordb.as_retriever(
-        search_type=SEARCH_TYPE,
-        search_kwargs=SEARCH_KWARGS
-    )
